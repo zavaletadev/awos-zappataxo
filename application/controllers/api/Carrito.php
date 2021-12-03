4dc17b5981120087f35c088d455aba5790cf6709
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-12-02 22:44:17 -0600

    Ejemplo de gestión de carrito de compras

--- application/controllers/api/Carrito.php
+++ application/controllers/api/Carrito.php
@@ -95,14 +95,173 @@ class Carrito extends CI_Controller {
 
     /**
      * [lista description]
+     * @example [POST(id)] https://zavaletazea.dev/labs/awos-dapps-zappataxo/api/carrito/lista
      * @return [type] [description]
      */
     function lista()
     {
+
+        header('Content-Type: application/json; charset=utf-8');
+
         $this->form_validation->set_rules('id', 'id', 'trim|required|exact_length[32]');
 
         if ($this->form_validation->run()) {
+            $id = $this->input->post('id');
 
+            /*
+            Tomamos el id sin ecriptar a partir del id encriptado
+             */
+            $productos_carrito = $this->carrito_model->lista(
+                $this->auth_model->get_id_by_md5id($id)
+            );
+
+            /*
+            Recorremos a productos carrito
+             */
+            $total = 0;
+            foreach ($productos_carrito as $pc) {
+                $subtotal = ((double)$pc->cantidad * (double) $pc->precio_prod);
+                $total += $subtotal;
+            }
+
+            echo json_encode(
+                array(
+                    'code' => 200, 
+                    'data' => $productos_carrito,
+                    'total' => '$'.number_format($total, 2, '.', ',').' MXN'
+                )
+            );
+        }
+
+        /*
+        Si la validación no se cumple
+         */
+        else {
+            //Eliminamos las etiquetas de los códigos de error
+            $this->form_validation->set_error_delimiters('', '');
+            echo json_encode(
+                array(
+                    'code' => 400, 
+                    'errors' => validation_errors()
+                )
+            );
+        }
+    }
+
+    /**
+     * [eliminar description]
+     * @example [POST(id,carrito_id)] https://zavaletazea.dev/labs/awos-dapps-zappataxo/api/carrito/eliminar
+     * @return [type] [description]
+     */
+    function eliminar()
+    {
+
+        header('Content-Type: application/json; charset=utf-8');
+
+        $this->form_validation->set_rules('id', 'id', 'trim|required|exact_length[32]');
+        $this->form_validation->set_rules('carrito_id', 'carrito_id', 'trim|required|integer');        
+
+        if ($this->form_validation->run()) {
+            $id         = $this->input->post('id');
+            $carrito_id = $this->input->post('carrito_id');
+
+            /*
+            Eliminamos el producto del carrito
+             */
+            $this->carrito_model->eliminar($carrito_id);
+
+            /*
+            Tomamos el id sin ecriptar a partir del id encriptado
+             */
+            $productos_carrito = $this->carrito_model->lista(
+                $this->auth_model->get_id_by_md5id($id)
+            );
+
+            /*
+            Recorremos a productos carrito
+             */
+            $total = 0;
+            foreach ($productos_carrito as $pc) {
+                $subtotal = ((double)$pc->cantidad * (double) $pc->precio_prod);
+                $total += $subtotal;
+            }
+
+            echo json_encode(
+                array(
+                    'code' => 200, 
+                    'data' => $productos_carrito,
+                    'total' => '$'.number_format($total, 2, '.', ',').' MXN'
+                )
+            );
+        }
+
+        /*
+        Si la validación no se cumple
+         */
+        else {
+            //Eliminamos las etiquetas de los códigos de error
+            $this->form_validation->set_error_delimiters('', '');
+            echo json_encode(
+                array(
+                    'code' => 400, 
+                    'errors' => validation_errors()
+                )
+            );
+        }
+    }
+
+    /**
+     * [eliminar description]
+     * @example [POST(id,carrito_id,cantidad)] https://zavaletazea.dev/labs/awos-dapps-zappataxo/api/carrito/actualizar
+     * @return [type] [description]
+     */
+    function actualizar()
+    {
+
+        header('Content-Type: application/json; charset=utf-8');
+        
+        $this->form_validation->set_rules('id', 'id', 'trim|required|exact_length[32]');
+        $this->form_validation->set_rules('carrito_id', 'carrito_id', 'trim|required|integer');        
+        $this->form_validation->set_rules('cantidad', 'cantidad', 'trim|required|numeric');        
+
+        if ($this->form_validation->run()) {
+            $id         = $this->input->post('id');
+            $carrito_id = $this->input->post('carrito_id');
+            $cantidad = $this->input->post('cantidad');
+
+            /*
+            Actualizamnos los productos del carrito
+             */
+            $this->carrito_model->actualizar(
+                array(
+                    "cantidad" => $cantidad
+                ),
+                $carrito_id
+            );
+
+            /*
+            Tomamos el id sin ecriptar a partir del id encriptado
+             */
+            $productos_carrito = $this->carrito_model->lista(
+                $this->auth_model->get_id_by_md5id($id)
+            );
+
+            /*
+            Recorremos a productos carrito
+             */
+            $total = 0;
+            foreach ($productos_carrito as $pc) {
+                $subtotal = ((double)$pc->cantidad * (double) $pc->precio_prod);
+                $total += $subtotal;
+            }
+
+            echo json_encode(
+                array(
+                    'code' => 200, 
+                    'data' => $productos_carrito,
+                    'total' => '$'.number_format($total, 2, '.', ',').' MXN'
+                )
+            );
         }
 
         /*
--- application/models/Carrito_model.php
+++ application/models/Carrito_model.php
@@ -3,13 +3,77 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Carrito_model extends CI_Model {
 
+    /**
+     * [agregar description]
+     * @param  [type] $data [description]
+     * @return [type]       [description]
+     */
     function agregar($data)
     {
         $this->db->insert('carrito', $data);
 
         return TRUE;
     }
-    
+
+    /**
+     * [lista description]
+     * @param  [type] $usuario_id [description]
+     * @return [type]             [description]
+     */
+    function lista($usuario_id)
+    {
+        //Creamos la consulta
+        /*
+        SELECT * FROM carrito 
+        JOIN producto USING(producto_id) 
+        WHERE producto.estatus = 1
+        AND usuario_id = '$usuario_id'
+         */
+        $this->db->select('*');
+        $this->db->from('carrito');
+        $this->db->join(
+            'producto', 
+            'carrito.producto_id = producto.producto_id', 
+            'left'
+        );
+        $this->db->where('producto.estatus', 1);
+        $this->db->where('carrito.usuario_id', $usuario_id);
+        $query = $this->db->get();
+
+        /*
+        Si no existen productos en el carrito para ese usuario 
+        retornamos un arreglo vacío
+         */
+        return $query->num_rows() > 0 ? $query->result() : array();
+    }
+
+    /**
+     * [eliminar description]
+     * @param  [type] $carrito_id [description]
+     * @return [type]             [description]
+     */
+    function eliminar($carrito_id)
+    {
+        $this->db->where('carrito_id', $carrito_id);        
+        $this->db->delete('carrito');
+
+        return TRUE;
+    }
+
+    /**
+     * [actualizar description]
+     * @param  [type] $data       [description]
+     * @param  [type] $carrito_id [description]
+     * @return [type]             [description]
+     */
+    function actualizar($data, $carrito_id)
+    {
+        $this->db->where('carrito_id', $carrito_id);        
+        $this->db->update('carrito', $data);
+
+        return TRUE;
+    }
+
 
 }
 
